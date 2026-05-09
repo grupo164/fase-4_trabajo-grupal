c538abf38e32b1a7a02b14f4954a68bb4a7320e2
katerinnavas43-alt <katerinnavas43@gmail.com>
2026-05-09 09:42:14 -0500

    Create proceso_reservas.py

---
+++ proceso_reservas.py
@@ -0,0 +1,8 @@
+# Importamos las clases necesarias para integrar el sistema
+from gestion_clientes import Cliente
+
+# Definimos la clase Reserva que conectará clientes con servicios
+class Reserva:
+    """
+    Clase que integra al cliente, el servicio y la duración.
+    Implementa
