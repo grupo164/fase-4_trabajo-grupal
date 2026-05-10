6b00179f4eccdc00c1e239adfed83a39dcbdcba3
DiegoTobosc15 <diegotobosc15@gmail.com>
2026-05-10 06:59:03 -0500

    Agregar configuración de logging y creación de directorio para logs en logger.py

--- logger.py
+++ logger.py
@@ -1,3 +1,13 @@
 import logging
 import os
 
+os.makedirs("logs", exist_ok=True)
+ 
+logging.basicConfig(
+    filename="logs/softwarefj.log",
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+ 
+logger = logging.getLogger("SoftwareFJ")
